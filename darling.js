dfddaadd85c235e93f35ce5238948b7fdb7b282b
Travis CI <ievgenii.krevenets@gmail.com>
2015-06-19 22:55:16 +0000

    bump version

--- darling.js
+++ darling.js
@@ -1,5 +1,5 @@
 /**
- * @license darlingjs v0.7.7 2015-06-19 by Eugene Krevenets.
+ * @license darlingjs v0.7.8 2015-06-19 by Eugene Krevenets.
  * Component & Entity based javascript game engine. Decoupled from any visualization, physics, and so on. With injections and modules based on AngularJS.
  * http://darlingjs.github.io/
  *
@@ -72,7 +72,7 @@ var _darlingutil = window.darlingutil;
  */
 var darlingutil = window.darlingutil = window.darlingutil||{};
 
-darlingutil.version = '0.7.7';
+darlingutil.version = '0.7.8';
 
 /**
  * @ngdoc function
@@ -1168,7 +1168,7 @@ var _darlingjs = window.darlingjs;
  * Uses for creating modules and game world.
  */
 var darlingjs = window.darlingjs || (window.darlingjs = {});
-darlingjs.version = '0.7.7';
+darlingjs.version = '0.7.8';
 
 var worlds = {};
 
